c14c33981f113a9125281c006c3a03acff10a7d8
leixun <conglomerate9@gmail.com>
2014-07-27 03:39:40 -0700

    side bar from Parse

--- js/index.js
+++ js/index.js
@@ -1,37 +1,103 @@
+//global declaration
+var Meme,Cat,meme_query,cat_query;
+
+/* called when page loads first time */
 window.onload = function(){
 
-Parse.initialize("EfZFq1ljZwCgYudEG4bkkLrSeUtJ9UxHyLHL6wYI", "DGtqZV6fc6o2hjn9UiuSJPe0p3dURcNv05hBgwFk");  
+  //associate parse account
+  Parse.initialize("EfZFq1ljZwCgYudEG4bkkLrSeUtJ9UxHyLHL6wYI", "DGtqZV6fc6o2hjn9UiuSJPe0p3dURcNv05hBgwFk");  
 
-var default_memes = [{src:"pic/meme1.png",name:"lol1"},{src:"pic/meme2.png",name:"lol2"},{src:"pic/meme3.png",name:"lol3"}];
+  //gloabal definition
+  Meme = Parse.Object.extend("Meme");
+  Cat = Parse.Object.extend("Cat");
+  meme_query = new Parse.Query(Meme);
+  cat_query = new Parse.Query(Cat);
 
-var Meme = Parse.Object.extend("Meme");
+  //setup default meme and cat (run only once)
+  //setupDefault();
 
-// for(i in default_memes){
-//   var meme = new Meme();
-//   meme.save(default_memes[i],{
-//     success: function(object) {
-//     // console.log("success");
-//   },
-//   error: function(model, error) {
-//     // console.log("error");
-//   }
-//   });
-// }
+  //insert contents
+  updateAllCat();
+  updateCatBar();
+};
 
-var query = new Parse.Query(Meme);
-query.find({
-  success: function(results){
-    for(i in results){
-      var cv = document.getElementById("content-view");
-      cv.innerHTML = cv.innerHTML+"<div class=\"memeImg\">"+
-            "<a href=\"newEdit.html\"><img src=\""+ results[i].attributes.src +"\" alt=\""+results[i].attributes.name+"\" height=\"150\" width=\"150\"></a>"+
-          "</div>";
+var setupDefault = function(){
+  var default_memes = [{src:"pic/meme1.png",name:"lol1",cat:"Cat1"},
+                       {src:"pic/meme2.png",name:"lol2",cat:"Cat2"},
+                       {src:"pic/meme3.png",name:"lol3",cat:"Cat3"}];
+  var default_cats = [{name:"Cat1", count:1},{name:"Cat2", count:1},{name:"Cat3", count:1}];
+  //SAVE DEFAULT MEME TO SERVER
+  for(i in default_memes){
+    var meme = new Meme();
+    meme.save(default_memes[i],{
+    success: function(object) {
+    },
+    error: function(model, error) {
     }
-  },
-  error: function(error){
-    alert("error");
+    });
   }
-})
+  //SAVE DEFAULT CAT TO SERVER
+  for(i in default_cats){
+    var cat = new Cat();
+    cat.save(default_cats[i],{
+    success: function(object) {
+    },
+    error: function(model, error) {
+    }
+    });
+  }
+};
+
+var updateAllCat = function(){
+  //insert memes
+  meme_query.find({
+    success: function(results){
+      var cv = document.getElementById("content-view");
+      for(i in results){
+        cv.innerHTML = cv.innerHTML+"<div class=\"memeImg\">"+
+              "<a href=\"newEdit.html\"><img src=\""+results[i].attributes.src+"\" alt=\""
+              +results[i].attributes.name+"\" data-id=\""+results[i].id
+              +"\" height=\"150\" width=\"150\"></a>"+
+              "</div>";
+      }
+      //attach event listener
+      var meme_div = document.getElementsByClassName('memeImg');
+      for(i=0;i<meme_div.length;i++){
+        meme_div[i].addEventListener("click",updateEdit,false);
+      }
+    },
+    error: function(error){
+      alert("updateAllCat error");
+    }
+  });
+};
+
+var updateCatBar = function(){
+  cat_query.find({
+    success: function(results){
+      var cb = document.getElementById("cat-bar");
+      for(i in results){
+        cb.innerHTML = cb.innerHTML+"<a href=\"indexCat2.html\">"
+        +"<div class=\"category-item\" data-name=\""+results[i].attributes.name
+        +"\" data-id=\""+results[i].id+"\">"+results[i].attributes.name+"</div>"
+      }
+      //attach event listener
+      var cat_div = document.getElementsByClassName('category-item');
+      for(i=0;i<cat_div.length;i++){
+        cat_div[i].addEventListener("click",updateCatView,false);
+      }
+    },
+    error: function(error){
+      alert("updateCatBar error");
+    }
+  });
+};
+
+var updateEdit = function(){
+  // alert("hihihi");
+};
+
+var updateCatView = function(){
 
 }
 
