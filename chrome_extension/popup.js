95c2d08f339aa2cea79f8c427521ae3c1191bbd5
stevotennis <stevotennis@gmail.com>
2014-07-30 09:12:53 -0700

    added IDs

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,2 +1,12 @@
-var tst = document.getElementById("meme_url").value;
+var sMeme_url = document.getElementById("meme_url").value;
+var sMeme_name = document.getElementById("meme_name").value;
+var sMeme_tag = document.getElementById("meme_tag").value;
+var star = document.getElementById("star").value;
+var meme_comment = document.getElementById("meme_comment").value;
 // $('#add_button').click( function() { alert('clicked'); });
+
+// save all the info into DB
+function saveMeme(){
+	alert('clicked ' + tst);
+
+}
