3eba6fbfca2883ae531eecc85f7807325f1fa6d3
stevotennis <stevotennis@gmail.com>
2014-07-30 00:56:40 -0700

    testing ext

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,9 +1,2 @@
-chrome.runtime.onMessage.addListener(function(message, sender) { 
-    if (message == 'showbar') {
-        // Note: When the API reaches the stable status, remove ".experimental"
-        chrome.experimental.infobars.show({
-            tabId: sender.tab.id,
-            path: 'infobar.html#' + sender.tab.id
-        });
-    }
-});
+var tst = document.getElementById("meme_url").value;
+// $('#add_button').click( function() { alert('clicked'); });
